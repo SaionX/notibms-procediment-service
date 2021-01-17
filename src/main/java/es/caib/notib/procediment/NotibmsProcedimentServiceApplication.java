430fd21cd1b3476c5aae386daf2e8a8ce174923b
sion <sion.andreu@gmail.com>
2021-01-17 19:07:52 +0100

    Pujada inicial

---
+++ src/main/java/es/caib/notib/procediment/NotibmsProcedimentServiceApplication.java
@@ -0,0 +1,13 @@
+package es.caib.notib.procediment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibmsProcedimentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotibmsProcedimentServiceApplication.class, args);
+	}
+
+}
